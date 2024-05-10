2f8ee5b55d99fbb08962449aa520af62a218d5a0
Nick Sen <nicksen223@gmail.com>
2024-05-10 20:19:35 +0800

    updated statistical_analysis file

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import numpy as np
 import pandas as pd
+import statistical_analysis
 
 
 data = pd.read_csv('./Data/dataset.csv')
-meta_data = data[['track_id', 'artists', 'track_name', 'album_name', 'popularity', 'duration_ms']]
-music_data = data[['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'time_signature']]
-print(meta_data.head())
-print(music_data.head())
-print(data.columns.tolist)
 
-
-print("Hello")
-print("ByeBye")
-
-
-print("this is surya")
+statistical_analysis.stats_analyzer(data)
---
+++ statistical_analysis.py
@@ -0,0 +1,6 @@
+def stats_analyzer(data_frame):
+    copy = data_frame
+    np_music_data = copy.to_numpy()
+    print(np_music_data)
+    print(type(np_music_data))
+    
